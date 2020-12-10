1a0a91ebd28f944dfc1d216e8165876dab4f71ea
Angelina Draguts <onlyyangle@gmail.com>
2020-12-10 13:59:14 +0300

    Anniversary lab is done! Congrats!

---
+++ Lab9_OOP/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9_OOP
+{
+    class Program
+    {
+        public delegate void Message();
+        abstract class Info
+        {
+            public virtual void Addiction() { }
+            public int Salary;
+            public int Expirience;
+            public static int Fool;
+
+            public string Name { get; set; }
+            public Info(string name)
+            {
+                Name = name;
+            }
+            public virtual void Display()
+            {
+                Console.WriteLine($"This employee's name is {Name}");
+            }
+            public class WorkString
+            {
+                public static string RemoveString(string str)  //удаление знаков препинания
+                {
+                    char[] signs = { '.', ',', '!', '?', '-', ':' };
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        if (signs.Contains(str[i]))
+                        {
+                            str = str.Remove(i, 1);
+                        }
+                    }
+                    return str;
+                }
+                public static string AddString(string str)  //добавление подстроки
+                {
+                    return str += "Дополнительная строка добавлена в исзодную";
+                }
+                public static string UpString(string str)  //перевод в верхний регистр
+                {
+                    return str.ToUpper();
+                }
+                public static string RemovePr(string str)  //удаление
+                {
+                    return str.Replace(" ", "");
+                }
+                public static string LowString(string str)  //нижний регистр
+                {
+                    return str.ToLower();
+                }
+            }
+            public class Worker : Info
+            {
+                //Событие Notify c типом делегата Message.
+                public event Message Notify;
+                public Worker(string name, int fool) : base(name)
+                {
+                    Fool = fool;
+                }
+                public string Position;
+                public Worker(string name, string pos, int exp, int fool, int salary) : base(name)
+                {
+                    Position = pos;
+                    Expirience = exp;
+                    Fool = fool;
+                    Salary = salary;
+                }
+                public override void Display()
+                {
+                    Console.WriteLine($"This employee's name is {Name} and his position is {Position}: the salary is {Salary}");
+                }
+
+                public override void Addiction()       //метод добавления бонусов
+                {
+                    if (Expirience > 3)
+                    {
+                        Salary += 150;
+                        Notify();                       //запускаю событие в условиях кода
+                        Console.WriteLine($"{Name} has bonus to salary");
+                    }
+                }
+            }
+            public class Student : Info
+            {
+
+                public event Message Notify2;
+                public int Kurs;
+                public override void Display()
+                {
+                    Console.WriteLine($"This student's name is {Name} and salary is {Salary}");
+                }
+
+                public override void Addiction()  //метод добавления бонусов
+                {
+                    if (Expirience > 3)
+                    {
+                        Salary += 150;
+                        Notify2();  //запускаю событие в условиях кода
+                        Console.WriteLine($"{Name} has bonus to salary");
+                    }
+                }
+                public Student(string name, int kurs) : base(name)
+                {
+                    Kurs = kurs;
+                }
+                public string Position;
+
+                public Student(string name, string pos, int exp, int fool, int salary) : base(name)
+                {
+                    Position = pos;
+                    Expirience = exp;
+                    Fool = fool;
+                    Salary = salary;
+                }
+
+            }
+            public class Director : Info
+            {
+
+                public int Unic;
+                public Director(string name, int unic) : base(name)
+                {
+                    Unic = unic;
+                }
+
+                static void Main(string[] args)
+                {
+
+                    Worker One = new Worker("Angelina", "Student", 8, 2, 2056);
+                    Worker Two = new Worker("Ben", "Engineer", 6, 0, 1890);
+                    Student Four = new Student("Sam", "Student", 6, 5, 1508);
+                    Student Five = new Student("Caroline", "Student", 0, 0, 1067);
+
+                    One.Display();
+                    Two.Display();
+                    Four.Display();
+                    Five.Display();
+
+                    Console.WriteLine("----------------------------------------------");
+
+                    One.Notify += One.Display;
+                    Two.Notify += Two.Display;
+                    Four.Notify2 += Four.Display;
+                    Five.Notify2 += Five.Display;
+
+
+                    One.Addiction();
+                    Two.Addiction();
+                    Four.Addiction();
+                    Five.Addiction();
+
+                    Console.WriteLine("----------------------------------------------");
+
+                    Func<string, string> work = WorkString.RemoveString; //стандартный делегат Func
+                    string str = Console.ReadLine();
+                    Console.WriteLine($"\nBefore: {str}\nAfter: {str = work(str)}\n");
+                    work = WorkString.UpString;
+                    Console.WriteLine($"\nBefore: {str}\nAfter: { str = work(str)}\n");
+                    work = WorkString.AddString;
+                    Console.WriteLine($"\nBefore: {str}\nAfter: {str = work(str)}\n");
+                    work = WorkString.RemovePr;
+                    Console.WriteLine($"\nBefore: {str}\nAfter: {str = work(str)}\n");
+                    work = WorkString.LowString;
+                    Console.WriteLine($"\nBefore: {str}\nAfter: {str = work(str)}\n");
+
+                }
+            }
+        }
+    }
+}
---
+++ Lab9_OOP/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Lab9_OOP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab9_OOP")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("63edad24-93f9-4759-b58a-433b1c7ee72f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
